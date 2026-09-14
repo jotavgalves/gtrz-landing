cdd4781c898f5644f42b60e6802df74cc03b8f10
jotavgalves <jvgacontato@gmail.com>
2026-09-14 14:24:14 -0300

    style(admin): load security and topbar styles

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './media.css';
+import './extra.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
